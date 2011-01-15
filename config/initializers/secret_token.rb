405ae68f6956a0b6f252b0ed8b0a5b0aa79f98bb
Neil J Peterson <github@x5w.org>
2011-01-15 13:41:03 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloNeil::Application.config.secret_token = '144bcf44a0332a2bc767ce6531669ec40f3a5c353fd1cb202384232f2d9dfd14bc0489d5e536e78d36e33a4986746e73f2776ec563637d38d03984430d00e537'
