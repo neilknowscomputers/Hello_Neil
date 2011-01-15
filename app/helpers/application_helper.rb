03d83f9dd5f71a6328193c190e1a0fb8e70d442e
Neil J Peterson <github@x5w.org>
2011-01-15 17:37:31 -0600

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # return a title
+  def title
+      base_title = "Hello Neil"
+      if @title.nil?
+        "#{base_title}"
+      else
+          "#{base_title} | #{@title}"
+      end
+  end        
 end
